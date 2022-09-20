c8acb80f5977ee12c84b7471da47865311ba664d
Dakota Hernandez <dhernan881@gmail.com>
2022-09-20 01:54:26 -0400

    he approaches.

--- PlayMode.cpp
+++ PlayMode.cpp
@@ -8,31 +8,56 @@
 #include "gl_errors.hpp"
 #include "data_path.hpp"
 
+#include <glm/gtc/quaternion.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
 #include <random>
+#include <iostream>
 
-GLuint hexapod_meshes_for_lit_color_texture_program = 0;
-Load< MeshBuffer > hexapod_meshes(LoadTagDefault, []() -> MeshBuffer const * {
-	MeshBuffer const *ret = new MeshBuffer(data_path("hexapod.pnct"));
-	hexapod_meshes_for_lit_color_texture_program = ret->make_vao_for_program(lit_color_texture_program->program);
+// GLuint hexapod_meshes_for_lit_color_texture_program = 0;
+// Load< MeshBuffer > hexapod_meshes(LoadTagDefault, []() -> MeshBuffer const * {
+// 	MeshBuffer const *ret = new MeshBuffer(data_path("hexapod.pnct"));
+// 	hexapod_meshes_for_lit_color_texture_program = ret->make_vao_for_program(lit_color_texture_program->program);
+// 	return ret;
+// });
+
+GLuint amogus_meshes_for_lit_color_texture_program = 0;
+Load< MeshBuffer > amogus_meshes(LoadTagDefault, []() -> MeshBuffer const * {
+	MeshBuffer const *ret = new MeshBuffer(data_path("amogus.pnct"));
+	amogus_meshes_for_lit_color_texture_program = ret->make_vao_for_program(lit_color_texture_program->program);
 	return ret;
 });
 
-Load< Scene > hexapod_scene(LoadTagDefault, []() -> Scene const * {
-	return new Scene(data_path("hexapod.scene"), [&](Scene &scene, Scene::Transform *transform, std::string const &mesh_name){
-		Mesh const &mesh = hexapod_meshes->lookup(mesh_name);
+// Load< Scene > hexapod_scene(LoadTagDefault, []() -> Scene const * {
+// 	return new Scene(data_path("hexapod.scene"), [&](Scene &scene, Scene::Transform *transform, std::string const &mesh_name){
+// 		Mesh const &mesh = hexapod_meshes->lookup(mesh_name);
+
+// 		scene.drawables.emplace_back(transform);
+// 		Scene::Drawable &drawable = scene.drawables.back();
+
+// 		drawable.pipeline = lit_color_texture_program_pipeline;
+
+// 		drawable.pipeline.vao = hexapod_meshes_for_lit_color_texture_program;
+// 		drawable.pipeline.type = mesh.type;
+// 		drawable.pipeline.start = mesh.start;
+// 		drawable.pipeline.count = mesh.count;
+
+// 	});
+// });
+
+Load< Scene > amogus_scene(LoadTagDefault, []() -> Scene const * {
+	return new Scene(data_path("amogus.scene"), [&](Scene &scene, Scene::Transform *transform, std::string const &mesh_name) {
+		Mesh const &mesh = amogus_meshes->lookup(mesh_name);
 
 		scene.drawables.emplace_back(transform);
 		Scene::Drawable &drawable = scene.drawables.back();
 
 		drawable.pipeline = lit_color_texture_program_pipeline;
 
-		drawable.pipeline.vao = hexapod_meshes_for_lit_color_texture_program;
+		drawable.pipeline.vao = amogus_meshes_for_lit_color_texture_program;
 		drawable.pipeline.type = mesh.type;
 		drawable.pipeline.start = mesh.start;
 		drawable.pipeline.count = mesh.count;
-
 	});
 });
 
@@ -40,28 +65,48 @@ Load< Sound::Sample > dusty_floor_sample(LoadTagDefault, []() -> Sound::Sample c
 	return new Sound::Sample(data_path("dusty-floor.opus"));
 });
 
-PlayMode::PlayMode() : scene(*hexapod_scene) {
+PlayMode::PlayMode() : scene(*amogus_scene) {
 	//get pointers to leg for convenience:
-	for (auto &transform : scene.transforms) {
-		if (transform.name == "Hip.FL") hip = &transform;
-		else if (transform.name == "UpperLeg.FL") upper_leg = &transform;
-		else if (transform.name == "LowerLeg.FL") lower_leg = &transform;
-	}
-	if (hip == nullptr) throw std::runtime_error("Hip not found.");
-	if (upper_leg == nullptr) throw std::runtime_error("Upper leg not found.");
-	if (lower_leg == nullptr) throw std::runtime_error("Lower leg not found.");
+	// for (auto &transform : scene.transforms) {
+	// 	if (transform.name == "Hip.FL") hip = &transform;
+	// 	else if (transform.name == "UpperLeg.FL") upper_leg = &transform;
+	// 	else if (transform.name == "LowerLeg.FL") lower_leg = &transform;
+	// }
+	// if (hip == nullptr) throw std::runtime_error("Hip not found.");
+	// if (upper_leg == nullptr) throw std::runtime_error("Upper leg not found.");
+	// if (lower_leg == nullptr) throw std::runtime_error("Lower leg not found.");
 
-	hip_base_rotation = hip->rotation;
-	upper_leg_base_rotation = upper_leg->rotation;
-	lower_leg_base_rotation = lower_leg->rotation;
+	// hip_base_rotation = hip->rotation;
+	// upper_leg_base_rotation = upper_leg->rotation;
+	// lower_leg_base_rotation = lower_leg->rotation;
+	
 
 	//get pointer to camera for convenience:
 	if (scene.cameras.size() != 1) throw std::runtime_error("Expecting scene to have exactly one camera, but it has " + std::to_string(scene.cameras.size()));
 	camera = &scene.cameras.front();
 
+	// Spawn dummy enemy for now
+	Mesh const &mesh = amogus_meshes->lookup("Amogus.Green");
+	Scene::Transform *new_trans = new Scene::Transform();
+	new_trans->name = "Astronaut0";
+	scene.drawables.emplace_back(new_trans);
+	Scene::Drawable &drawable = scene.drawables.back();
+	drawable.pipeline = lit_color_texture_program_pipeline;
+	drawable.pipeline.vao = amogus_meshes_for_lit_color_texture_program;
+	drawable.pipeline.type = mesh.type;
+	drawable.pipeline.start = mesh.start;
+	drawable.pipeline.count = mesh.count;
+	std::cout << "created transform and added to drawables" << std::endl;
+	Enemy *new_enemy = new Enemy {};
+	new_enemy->transform = new_trans;
+	new_enemy->type = EnemyType::GREEN;
+	new_enemy->dest_queue.push_back(camera->transform);
+	active_enemies[0] = new_enemy;
+	std::cout << "dummy enemy created" << std::endl;
+
 	//start music loop playing:
 	// (note: position will be over-ridden in update())
-	leg_tip_loop = Sound::loop_3D(*dusty_floor_sample, 1.0f, get_leg_tip_position(), 10.0f);
+	leg_tip_loop = Sound::loop_3D(*dusty_floor_sample, 1.0f, glm::vec3(0.0f), 10.0f);
 }
 
 PlayMode::~PlayMode() {
@@ -89,6 +134,18 @@ bool PlayMode::handle_event(SDL_Event const &evt, glm::uvec2 const &window_size)
 			down.downs += 1;
 			down.pressed = true;
 			return true;
+		} else if (evt.key.keysym.sym == SDLK_1) {
+			one.downs += 1;
+			one.pressed = true;
+			return true;
+		} else if (evt.key.keysym.sym == SDLK_2) {
+			two.downs += 1;
+			two.pressed = true;
+			return true;
+		} else if (evt.key.keysym.sym == SDLK_3) {
+			three.downs += 1;
+			down.pressed = true;
+			return true;
 		}
 	} else if (evt.type == SDL_KEYUP) {
 		if (evt.key.keysym.sym == SDLK_a) {
@@ -102,6 +159,15 @@ bool PlayMode::handle_event(SDL_Event const &evt, glm::uvec2 const &window_size)
 			return true;
 		} else if (evt.key.keysym.sym == SDLK_s) {
 			down.pressed = false;
+			return true;
+		} else if (evt.key.keysym.sym == SDLK_1) {
+			one.pressed = false;
+			return true;
+		} else if (evt.key.keysym.sym == SDLK_2) {
+			two.pressed = false;
+			return true;
+		} else if (evt.key.keysym.sym == SDLK_3) {
+			three.pressed = false;
 			return true;
 		}
 	} else if (evt.type == SDL_MOUSEBUTTONDOWN) {
@@ -127,27 +193,57 @@ bool PlayMode::handle_event(SDL_Event const &evt, glm::uvec2 const &window_size)
 	return false;
 }
 
+// Quaternion lookat function from https://stackoverflow.com/a/49824672
+glm::quat safe_quat_lookat(glm::vec3 const &fromPos, glm::vec3 const &toPos, 
+		glm::vec3 const &rotateAround = glm::vec3(0.0f, 0.0f, 1.0f)) {
+	glm::vec3 dir = toPos - fromPos;
+	float dir_len = glm::length(dir);
+
+	// Make sure dir is valid
+	if (!(dir_len > 0.0001f)) {
+		return glm::quat(1, 0, 0, 0); // identity
+	}
+
+	dir /= dir_len;
+	
+	// quatLookAt requires that dir is *not* parallel to the axis to rotate around.
+	if (glm::abs(glm::dot(dir, rotateAround)) > 0.9999f) {
+		return glm::quat(1, 0, 0, 0); // identity (I don't expect this to happen tbh)
+	}
+	return glm::quatLookAt(dir, rotateAround);
+}
+
+void PlayMode::Enemy::update(float elapsed) {
+	// update position
+	// TODO: add popping from dest queue and updating dest and looking at next dest
+
+	Scene::Transform *dest = dest_queue.front();
+	glm::vec3 move_dir = dest->position - transform->position;
+	move_dir = glm::normalize(move_dir);
+	transform->position = transform->position + (move_dir * Enemy::MoveSpeed * elapsed);
+}
+
 void PlayMode::update(float elapsed) {
 
 	//slowly rotates through [0,1):
-	wobble += elapsed / 10.0f;
-	wobble -= std::floor(wobble);
+	// wobble += elapsed / 10.0f;
+	// wobble -= std::floor(wobble);
 
-	hip->rotation = hip_base_rotation * glm::angleAxis(
-		glm::radians(5.0f * std::sin(wobble * 2.0f * float(M_PI))),
-		glm::vec3(0.0f, 1.0f, 0.0f)
-	);
-	upper_leg->rotation = upper_leg_base_rotation * glm::angleAxis(
-		glm::radians(7.0f * std::sin(wobble * 2.0f * 2.0f * float(M_PI))),
-		glm::vec3(0.0f, 0.0f, 1.0f)
-	);
-	lower_leg->rotation = lower_leg_base_rotation * glm::angleAxis(
-		glm::radians(10.0f * std::sin(wobble * 3.0f * 2.0f * float(M_PI))),
-		glm::vec3(0.0f, 0.0f, 1.0f)
-	);
+	// hip->rotation = hip_base_rotation * glm::angleAxis(
+	// 	glm::radians(5.0f * std::sin(wobble * 2.0f * float(M_PI))),
+	// 	glm::vec3(0.0f, 1.0f, 0.0f)
+	// );
+	// upper_leg->rotation = upper_leg_base_rotation * glm::angleAxis(
+	// 	glm::radians(7.0f * std::sin(wobble * 2.0f * 2.0f * float(M_PI))),
+	// 	glm::vec3(0.0f, 0.0f, 1.0f)
+	// );
+	// lower_leg->rotation = lower_leg_base_rotation * glm::angleAxis(
+	// 	glm::radians(10.0f * std::sin(wobble * 3.0f * 2.0f * float(M_PI))),
+	// 	glm::vec3(0.0f, 0.0f, 1.0f)
+	// );
 
-	//move sound to follow leg tip position:
-	leg_tip_loop->set_position(get_leg_tip_position(), 1.0f / 60.0f);
+	// //move sound to follow leg tip position:
+	// leg_tip_loop->set_position(get_leg_tip_position(), 1.0f / 60.0f);
 
 	//move camera:
 	{
@@ -176,6 +272,14 @@ void PlayMode::update(float elapsed) {
 		glm::vec3 frame_right = frame[0];
 		glm::vec3 frame_at = frame[3];
 		Sound::listener.set_position_right(frame_at, frame_right, 1.0f / 60.0f);
+	}
+
+	// update enemies:
+	{
+		for (auto &enemy : active_enemies) {
+			if (enemy != nullptr)
+				enemy->update(elapsed);
+		}
 	}
 
 	//reset button press counters:
@@ -226,11 +330,16 @@ void PlayMode::draw(glm::uvec2 const &drawable_size) {
 			glm::vec3(-aspect + 0.1f * H + ofs, -1.0 + + 0.1f * H + ofs, 0.0),
 			glm::vec3(H, 0.0f, 0.0f), glm::vec3(0.0f, H, 0.0f),
 			glm::u8vec4(0xff, 0xff, 0xff, 0x00));
+		// draw crosshair? ask later.
+		// lines.draw_text("+",
+		// 	glm::vec3(float(drawable_size.x) / 2.0f, float(drawable_size.y) / 2.0f, 0.0),
+		// 	glm::vec3(0.0f), glm::vec3(0.0f),
+		// 	glm::u8vec4(0x00, 0x00, 0x00, 0x00));
 	}
 	GL_ERRORS();
 }
 
-glm::vec3 PlayMode::get_leg_tip_position() {
-	//the vertex position here was read from the model in blender:
-	return lower_leg->make_local_to_world() * glm::vec4(-1.26137f, -11.861f, 0.0f, 1.0f);
-}
+// glm::vec3 PlayMode::get_leg_tip_position() {
+// 	//the vertex position here was read from the model in blender:
+// 	return lower_leg->make_local_to_world() * glm::vec4(-1.26137f, -11.861f, 0.0f, 1.0f);
+// }
--- PlayMode.hpp
+++ PlayMode.hpp
@@ -23,21 +23,47 @@ struct PlayMode : Mode {
 	struct Button {
 		uint8_t downs = 0;
 		uint8_t pressed = 0;
-	} left, right, down, up;
+	} left, right, down, up, one, two, three;
+
+	enum EnemyType {
+		DEFAULT = 0,
+		RED = 1,
+		GREEN = 2,
+		YELLOW = 3
+	};
+	struct Enemy {
+		// Properties
+		static constexpr float MoveSpeed = 10.0f;
+
+		// Methods
+		void update(float elapsed);
+
+		Scene::Transform *transform = nullptr;
+		std::deque< Scene::Transform* > dest_queue = {};
+		EnemyType type = EnemyType::DEFAULT;
+		uint enemy_list_idx = 0;
+	};
+	static constexpr uint MaxEnemies = 15;
+	PlayMode::Enemy *active_enemies[PlayMode::MaxEnemies] = 
+		{nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, 
+		nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr};
+	std::deque< uint > enemies_to_delete;
 
 	//local copy of the game scene (so code can change it during gameplay):
 	Scene scene;
 
 	//hexapod leg to wobble:
-	Scene::Transform *hip = nullptr;
-	Scene::Transform *upper_leg = nullptr;
-	Scene::Transform *lower_leg = nullptr;
-	glm::quat hip_base_rotation;
-	glm::quat upper_leg_base_rotation;
-	glm::quat lower_leg_base_rotation;
-	float wobble = 0.0f;
+	// Scene::Transform *hip = nullptr;
+	// Scene::Transform *upper_leg = nullptr;
+	// Scene::Transform *lower_leg = nullptr;
+	// glm::quat hip_base_rotation;
+	// glm::quat upper_leg_base_rotation;
+	// glm::quat lower_leg_base_rotation;
+	// float wobble = 0.0f;
+	// glm::vec3 get_leg_tip_position();
 
-	glm::vec3 get_leg_tip_position();
+	Scene::Transform *amogus = nullptr;
+
 
 	//music coming from the tip of the leg (as a demonstration):
 	std::shared_ptr< Sound::PlayingSample > leg_tip_loop;
